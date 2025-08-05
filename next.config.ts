15385c3c11b38fae9029c4491fb19cae335ff77f
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2025-08-05 09:59:28 +0700

    add pages and redirection rules

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/garage",
+        destination: "/garage/vehicle-types",
+        permanent: true,
+      },
+      {
+        source: "/activities",
+        destination: "/activities/competition",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
