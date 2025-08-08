6a3f04ec9816ae564040e2ef5bcc13e7474c9e14
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2025-08-09 00:17:41 +0700

    update hero section based on the new figma design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,6 +7,8 @@ import {
 } from "./ui/carousel";
 import Image from "next/image";
 import Autoplay from "embla-carousel-autoplay";
+import Link from "next/link";
+import { ChevronRightCircleIcon } from "lucide-react";
 
 const heroImages = [
   { path: "/hero_image_1.jpg", alt: "Nogogeni VII" },
@@ -34,7 +36,7 @@ function Hero() {
           {heroImages.map((img) => (
             <CarouselItem
               key={img.alt}
-              className="relative h-72 tablet:h-[512px] desktop:h-[calc(100vh-96px)]"
+              className="relative h-[calc(100vh-64px)] desktop:h-[calc(100vh-96px)]"
             >
               <Image
                 fill
@@ -48,16 +50,27 @@ function Hero() {
         </CarouselContent>
       </Carousel>
 
-      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex items-center gap-3 desktop:gap-4">
-        {Array.from({ length: 3 }).map((_, index) => (
-          <button
-            key={index}
-            onClick={() => carouselApi?.scrollTo(index)}
-            className={`rounded-full transition-colors cursor-pointer w-3 h-3 desktop:w-4 desktop:h-4 ${
-              index + 1 === current ? "bg-nogogeni-orange" : "bg-nogogeni-white"
-            }`}
-          />
-        ))}
+      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex flex-col items-center gap-4">
+        <Link
+          href="/garage/vehicle-types"
+          className="text-sm font-semibold flex justify-center items-center gap-2 tablet:text-base tablet:gap-3 desktop:hidden"
+        >
+          Discover <ChevronRightCircleIcon className="size-4 tablet:size-5" />
+        </Link>
+
+        <div className="flex items-center gap-3 desktop:gap-4">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <button
+              key={index}
+              onClick={() => carouselApi?.scrollTo(index)}
+              className={`rounded-full transition-colors cursor-pointer w-3 h-3 desktop:w-4 desktop:h-4 ${
+                index + 1 === current
+                  ? "bg-nogogeni-orange"
+                  : "bg-nogogeni-white"
+              }`}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
